8b81a98609f33415f0b73783484ed02c85b90bb1
benjadevcodes <benja.codes@gmail.com>
2026-02-08 00:08:24 -0300

    fix of the variables names

--- main.rb
+++ main.rb
@@ -4,24 +4,24 @@ def ceaser_cipher(string_to_convert, cipher_key)
   string_already_split = string_to_convert.downcase.split("")
   already_cipher = []
   string_already_split.each do |string_word| 
-    alfabet.each_with_index do |alfabet_word, index|
+    alfabet.each_with_index do |alfabet_word, index_alphabet|
       if string_word == alfabet_word
-        dsq = (index + cipher_key) % 26
-        already_cipher.push(dsq)
+        word_already_cipher = (index_alphabet + cipher_key) % 26
+        already_cipher.push(word_already_cipher)
       end
     end
   end
 
-  sting_ciphered = []
-  already_cipher.each do |e|
-    alfabet.each_with_index do |x, y|
-      if e == y
-        sting_ciphered.push(x)
+  string_ciphered = []
+  already_cipher.each do |word_inside_already_cipher|
+    alfabet.each_with_index do |alfabet_word, index_alphabet|
+      if word_inside_already_cipher == index_alphabet
+        string_ciphered.push(alfabet_word)
       end
     end
   end
 
-  return puts sting_ciphered.join
+  return puts string_ciphered.join
 end
 
 
